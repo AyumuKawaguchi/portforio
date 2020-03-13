479e7642c95b12d2c1451f98ec3610cf6cab521d
mimisenn <53231640+mimisenn@users.noreply.github.com>
2020-03-13 17:35:46 +0900

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -41,3 +41,9 @@ preload_app!
 plugin :tmp_restart
 
 # rackup DefaultRackup
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
